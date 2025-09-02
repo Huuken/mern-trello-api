d69a6102441c83cd2626b246bae905b196de7033
NET_10_NguyenHuuCanh <canhnhce171635@fpt.edu.vn>
2025-09-03 02:11:13 +0700

    Add API get board details

--- src/routes/v1/boardRoute.js
+++ src/routes/v1/boardRoute.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import { StatusCodes } from 'http-status-codes'
 import { boardValidation } from '~/validations/boardValidation'
-import {boardController} from '~/controllers/boardController'
+import { boardController } from '~/controllers/boardController'
 
 const Router = express.Router()
 
@@ -10,5 +10,9 @@ Router.route('/')
     res.status(StatusCodes.OK).json({ massage: 'Note: API get list boards' })
   })
   .post(boardValidation.createNew, boardController.createNew)
+
+Router.route('/:id')
+  .get(boardController.getDetails)
+  .put() //update board
 
 export const boardRoute = Router
--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-useless-catch */
 import { slugify } from '~/utils/formatters'
 import { boardModel } from '~/models/boardModel'
+import ApiError from '~/utils/ApiError'
+import { StatusCodes } from 'http-status-codes'
 
 const createNew = async (data) => {
   try {
@@ -9,14 +11,25 @@ const createNew = async (data) => {
       ...data,
       slug: slugify(data.title)
     }
-
     //Redirect den tang model
     const createdBoard = await boardModel.createNew(newBoard)
-
     //Get board
     const getNewBoard = await boardModel.findBoardById(createdBoard.insertedId)
-
     return getNewBoard
+  } catch (error) {
+    throw error
+  }
+}
+
+const getDetails = async (boardId) => {
+  try {
+
+    //Redirect den tang model
+    const board = await boardModel.getDetails(boardId)
+    if (!board) {
+      throw new ApiError(StatusCodes.NOT_FOUND, 'Board not found!')
+    }
+    return board
   } catch (error) {
     throw error
   }
@@ -24,5 +37,6 @@ const createNew = async (data) => {
 }
 
 export const boardService = {
-  createNew
+  createNew,
+  getDetails
 }
