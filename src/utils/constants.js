408ac2c180af182e7010f04d1b0f8bac62a87c13
NguyenHuuCanh <nguyenhuucanh20032003@gmail.com>
2025-09-03 22:14:08 +0700

    Aggregate Query

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,8 @@ export const WHITELIST_DOMAINS = [
   'http://localhost:5173'
   //Sau nay deploy thi se them domains vao day
 ]
+
+export const BOARD_TYPES = {
+  PUBLIC: 'public',
+  PRIVATE: 'private'
+}
